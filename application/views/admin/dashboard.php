e94c3c5a617da528c5f4086a10afa1a1de2629e3
sufarman <110842122+sufarman@users.noreply.github.com>
2022-08-31 13:57:48 +0700

    Update Controller

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,1 +1,24 @@
+<<<<<<< HEAD
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Testing remote</title>
+</head>
+<body>
+    <h1> jay</h1>
+    <br>
+    <p> beneran bisa ga jay???</p>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto repellat esse accusantium libero magnam illum rem eos magni nihil? Doloribus repellat esse sequi facilis velit mollitia ullam animi nisi.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque ea sed, nisi ut assumenda velit eaque officia! Ipsa, officiis id fugit omnis illo consequatur ex, illum deleniti expedita fuga sapiente!</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum deserunt repellendus expedita quae fugit dolor assumenda eaque omnis iure nihil. Voluptatibus aliquid temporibus ex voluptate, distinctio ea impedit accusantium quidem!</p>
+    <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. In sunt doloribus facere cupiditate voluptas voluptatum tempore distinctio exercitationem reprehenderit dolor ea, aperiam perferendis saepe maiores quam commodi at. Quos, laboriosam!</p>
+    <br>
+</body>
+</html>
+=======
 asdasd
+>>>>>>> ba004d023349d782cd4308c4e667975bd9d8b146
